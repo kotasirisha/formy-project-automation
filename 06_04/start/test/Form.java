d48edf19a818740b646b0d97ca6ff14e91d5f7fd
Srinivas Sista <sista80@gmail.com>
2020-09-29 21:29:39 -0500

    Initial commit

---
+++ 06_04/start/test/Form.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.Assert.assertEquals;
+
+public class Form {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\kotas\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        FormPage formPage = new FormPage();
+        formPage.submitForm(driver);
+
+        ConfirmationPage confirmationPage = new ConfirmationPage();
+        confirmationPage.getAlertBannerText(driver);
+
+
+        waitForAlertBanner(driver);
+
+        assertEquals("The form was successfully submitted!", getAlertBannerText(driver));
+
+        driver.quit();
+    }
+
+
+
+}
